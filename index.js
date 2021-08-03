6f8dbea2b6cb6dbdc51c73b36b5a0264014a5452
AlexKynoch <67015973+AlexKynoch@users.noreply.github.com>
2021-08-03 16:12:42 +0100

    basic layout design

---
+++ index.js
@@ -0,0 +1,5 @@
+function start() {
+    document.getElementById("hideThis").style.display = "block";
+    document.getElementById("startBtn").style.display = "none";
+    document.getElementById("paragraph").style.display = "none";
+}
