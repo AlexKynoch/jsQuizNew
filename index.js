26a6814344060300d6fcdc52f7c341f8010e2259
AlexKynoch <67015973+AlexKynoch@users.noreply.github.com>
2021-08-04 14:02:56 +0100

    question order changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const questions = [["what color are bananas"],
 ["If you found a baby sparrow would you?"],
 ["what color are tomatos"]];
 
-const answers = [["yellow", "red", "pink", "purple"],
+const answers = [["A) yellow", "B) red", "C) pink", "D) purple"],
 ["grey", "green", "orange", "silver"],
 ["green", "red", "magenta", "jade"],
 ["A) Teach it to tweet.", "B) Ask it to be your friend.", "C) Tell it about your bird nurturing skills.", "D) Pin it to a board."],
@@ -26,15 +26,15 @@ function start() {
 }
 
 function populateQuestion() {
-    document.getElementById("question").innerHTML = questions[3][0];
+    document.getElementById("question").innerHTML = questions[0][0];
 
 }
 
 function populateAnswerOptions() {
-    document.getElementById("opt1").innerHTML = answers[3][0];  //give the id options the value of elements at these indexes in answers array
-    document.getElementById("opt2").innerHTML = answers[3][1];
-    document.getElementById("opt3").innerHTML = answers[3][2];
-    document.getElementById("opt4").innerHTML = answers[3][3];
+    document.getElementById("opt1").innerHTML = answers[0][0];  //give the id options the value of elements at these indexes in answers array
+    document.getElementById("opt2").innerHTML = answers[0][1];
+    document.getElementById("opt3").innerHTML = answers[0][2];
+    document.getElementById("opt4").innerHTML = answers[0][3];
 
     // document.getElementById("btn1").value = answers[0][0];  //give the buttons the value of elements at these indexes in answers array
     // document.getElementById("btn2").value = answers[0][1];
